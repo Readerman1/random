f63aa5ebefc5051a9e452c88fb1acfeb2003853d
Readerman1 <Delanofred@live.com>
2018-04-12 15:30:10 -0400

    lab 9

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,26 @@
 #include "ofApp.h"
-
+#include <random>
+#include <iostream>
+#include <vector>
+using namespace std;
 //--------------------------------------------------------------
 void ofApp::setup(){
-jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+	jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
+	float xmean=1000;
+	float xsd=15;
+	float ymean=800;
+	float ysd=15;
+	
+	std::random_device rd;
+	std::mt19937 eng1(rd());
+	std::normal_distribution <float> norm(xmean, xsd);
+	std::normal_distribution<float> norm1(ymean, ysd);
+for(int i=0 ; i<20;i++){
+X.push_back(norm(eng1));
+Y.push_back(norm1(eng1));
+}
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
 }
 
 //--------------------------------------------------------------
